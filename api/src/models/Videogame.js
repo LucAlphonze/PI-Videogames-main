563ec5f42c581f85593c048ef1fad89c0f1412ed
Lucas Alfonzo <lucasalfonzo19@gmail.com>
2022-03-29 10:20:23 -0300

    PI videogames

---
+++ api/src/models/Videogame.js
@@ -0,0 +1,39 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('videogame', {
+
+    id:{
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+      default: DataTypes.UUIDV4
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description:{
+      type: DataTypes.TEXT,
+    },
+    released:{
+      type: DataTypes.DATEONLY
+    },
+    rating:{
+      type:DataTypes.DECIMAL
+    },
+    platforms:{
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: false
+    },
+    image: {
+      type: DataTypes.STRING
+  }
+},
+    {
+      timestamps:false
+  });
+};
