2fcaf2443c81a956ddc0ad17979071f72d7fcc93
Lucas Alfonzo <lucasalfonzo19@gmail.com>
2022-04-11 00:00:12 -0300

    comiteo nocturno

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
-  getVideogames,
+    getVideogames,
     filterVideogamesByGenres,
     orderByName,
     filterCreated,
--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -4,14 +4,16 @@ import "../components/css/LandingPage.css"
 // import loadingLanding from "../components/css/Landing-gif.gif"
 export default function LandingPage() {
   return (
-    <div >
-      <div className="Background-landingPage">
-      <h1 className="title-landingPage">[The Videogame Project]</h1>
+    <div className="Background-landingPage">
+      <div className="title-landingPage">
+      <h1>[The Videogame Project]</h1>
       </div>
       {/* <img src={loadingLanding} className="loading" alt="please wait"></img> */}
+      <div className="starto">
       <Link to="/home">
         <button className="Button">S T A R T</button>
       </Link>
+      </div>
     </div>
   );
 }
